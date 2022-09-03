21435307a77e1c9cbc4c260e5d0797e669795ad3
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-09-03 11:55:40 -0500

    Ejemplos de funciones en dart.

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  print(obtenerNombre('Adrian'));
+  print(obtenerNombreApellido('Adrian', 'Villamil'));
+  print(obtenerNombreApellidoNameParameter(apellido: 'Villamil'));
+}
+
+String obtenerNombre(String valor) {
+  return 'Mi nombre es $valor';
+}
+
+String obtenerNombreApellido(String nombre, String apellido) {
+  return 'Mi nombre es $nombre $apellido';
+}
+
+String obtenerNombreApellidoNameParameter({ String? nombre, required String apellido, int edad = 0 }) {
+  return 'Mi nombre es $nombre $apellido';
+}
+
+var miFuncionAnonima = (String valor) {
+  return 'El valor es $valor';
+};
