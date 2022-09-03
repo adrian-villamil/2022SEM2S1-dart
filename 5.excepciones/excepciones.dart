c492c83adc97703b02d21ce32e50d17677680bc1
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-09-03 16:01:24 -0500

    Ejemplos de excepciones con dart.

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,24 @@
-void main() {}
+void main() {
+  try {
+    String elNombre = obtenerNombre('felipe');
+    print(elNombre);
+  } on FormatException catch (e) {
+    print('Se present{o una excepci{on FormatException');
+  } on Exception catch (e) {
+    print('Se present{o una excepción Exception');
+  } catch (e) {
+    print('Se present{o una excepción');
+  } finally {
+    print('Este es el finally');
+  }
+}
+
+String obtenerNombre(String nombre) {
+  if (nombre == 'adrian') {
+    throw FormatException('Este nombre no está permitido');
+  }
+  if (nombre == 'felipe') {
+    throw Exception('Este nombre no está permitido');
+  }
+  return 'Mi nombre es $nombre';
+}
